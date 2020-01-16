c1acd62a0c2ed932da5c6cf49b37b3348386e6cc
Mlodovico <murilo.lodovico@outlook.com>
2020-01-15 23:12:12 -0300

    0.0.2 tornando o backend suscetivel a web

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,10 +9,10 @@ mongoose.connect('mongodb+srv://omnistack:omnistack@cluster0-o7nae.mongodb.net/w
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
-
+app.use(cors())
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+app.listen(3330);
 
 
