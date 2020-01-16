b085ca42c4c6f0ddf096e9901365eb4f6509045a
Mlodovico <murilo.lodovico@outlook.com>
2020-01-15 23:16:23 -0300

    0.0.2 construindo website e dividindo corretamente os packs

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,49 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import api from './service/api';
+
 import './global.css';
 import './App.css';
-import './Sidebar.css'
+import './Sidebar.css';
+import './Main.css';
 
+import DevForm from './components/DevForm';
+import DevItem from './components/DevItem';
 
 function App(){
+  const [devs, setDevs] = useState([]);
 
+  useEffect(() =>{
+    async function loadDevs(){
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  })
+
+  async function handleAddDev(data){
+    const response = api.post('/devs',data);
+    
+    setDevs([...devs, response.data]);
+
+  }
 
   return ( 
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-        <form>
-          <div class="input-block">
-          <label htmlFor="">Usuário do Github</label>
-          <input name="github_username" id="github_username" required />
-          </div>
-
-          <div class="input-block">
-          <label htmlFor="">Usuário do Github</label>
-          <input name="techs" id="techs" required />
-          </div>
-
-          <div className="input-group">
-            <div class="input-block">
-            <label htmlFor="latitude">Latitude</label>
-            <input name="latitude" id="latitude" required />
-            </div>
-          </div>
-
-          <div className="input-group">
-            <div class="input-block">
-            <label htmlFor="longitude">Longitude</label>
-            <input name="longitude" id="longitude" required />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev}/>
       </aside>
       <main>
         <ul>
-          <li>
-            
-          </li>
+          {devs.map(dev =>(
+            <DevItem key={dev._id} dev={dev} />
+          ))}
+                    
         </ul>
       </main>
     </div>
